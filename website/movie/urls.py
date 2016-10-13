dc7eba11bcc98e7bc78ff8d3ed49a21ab74d5027
Supanat Pokturng <Supanat Pokturng>
2016-10-13 10:16:30 +0700

    add register api

---
+++ website/movie/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'movie'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.login, name="login"),
+    url(r'^register/$', views.register, name="register"),
+    url(r'^api/login/$', views.login_api, name="login_api"),
+    url(r'^api/register/$', views.register_api, name="register_api"),
+]
