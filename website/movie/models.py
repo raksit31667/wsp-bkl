f9c4549c219293b71683c8d257beb91c00519d25
raksit.m <rak-sit@hotmail.com>
2016-10-14 09:52:44 +0700

    Added movie name into the Movie table and name representations

--- website/movie/models.py
+++ website/movie/models.py
@@ -4,10 +4,14 @@ from django.contrib.auth.models import User
 
 class Genre(models.Model):
     genre_name = models.CharField(max_length=15)
-    genre_description = models.CharField(max_length=100)
+    genre_description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.genre_name
 
 class Movie(models.Model):
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    movie_name = models.CharField(max_length=300)
     movie_description = models.CharField(max_length=500)
     release_year = models.PositiveSmallIntegerField(default=0)
     upload_date = models.DateTimeField(auto_now_add=True)
@@ -16,4 +20,6 @@ class Movie(models.Model):
     movie_thumbnail = models.FileField()
     movie_file = models.FileField()
     user = models.ForeignKey(User, default=1)
-    
+
+    def __str__(self):
+        return self.movie_name
