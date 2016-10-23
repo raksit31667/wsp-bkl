9a84ab71aa47d9b0d551d53bdcefcc2e62001066
Pongsakorn Somsri <pongsakorns@gmail.com>
2016-10-23 12:08:17 +0700

    fix merge conflict in view

--- website/movie/views.py
+++ website/movie/views.py
@@ -117,12 +117,3 @@ def login_api(request):
         request.session['invalid_login'] = 'Invalid username or password.'
 
     return HttpResponseRedirect(reverse('movie:index'))
-
-def showMovie(request):
-    movies_all_genre = []
-    all_movies_6 = Movie.objects.all()[:6]
-    all_genres = Genre.objects.all()
-    for genre in all_genres:
-        movies_in_genre = Movie.objects.filter(genre=genre)[:6]
-        movies_all_genre.append(movies_in_genre)
-    return render(request,'viewMovie.html',{'all_movies_6':all_movies_6,'all_genres':all_genres,'movies_all_genre':movies_all_genre})
