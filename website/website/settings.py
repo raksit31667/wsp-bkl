fd37922d2ebbc6045956a0ac5cba4fed5431dedc
Pongsakorn Somsri <pongsakorns@gmail.com>
2016-10-23 12:18:13 +0700

    hotfix2 settings

--- website/website/settings.py
+++ website/website/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'wsp-bkl',
         'USER': 'postgres',
-        'PASSWORD': '50135013',
+        'PASSWORD': '',
         'HOST': '',
         'PORT': '',
     }
