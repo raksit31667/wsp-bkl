d4ab30c2b354ce8450b9996396c4f9f843bdb22a
raksit <rak-sit@hotmail.com>
2016-10-25 15:14:27 +0700

    search from typeahead by word

--- website/movie/urls.py
+++ website/movie/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^api/login/$', views.login_api, name="login_api"),
     url(r'^filter/$', views.filter, name="filter"),
+    url(r'^search/$', views.search_movie, name="search"),
     url(r'^download/([0-9]+)/$',views.download_movie, name='movie'),
 ]
