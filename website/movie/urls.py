cc5331ffe330e252324ebedfe1ad6b8fb6141931
raksit <rak-sit@hotmail.com>
2016-10-25 20:18:00 +0700

    modified sortby code and url regex

--- website/movie/urls.py
+++ website/movie/urls.py
@@ -5,7 +5,7 @@ app_name = 'movie'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^api/login/$', views.login_api, name="login_api"),
-    url(r'^filter//?genre_id=(?P<genre_id>[0-9]+)/$', views.filter, name="filter"),
+    url(r'^filter//?genre=(?P<genre>[\w\-]+)//?sortby=(?P<sortby>[\w\-]+)/$', views.filter, name="filter"),
     url(r'^search/$', views.search_movie, name="search"),
     url(r'^download/([0-9]+)/$',views.download_movie, name='movie'),
 ]
