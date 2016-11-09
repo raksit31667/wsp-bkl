2a916af34959d6ade959a221a85c435948dcbf5e
raksit <rak-sit@hotmail.com>
2016-11-09 15:30:31 +0700

    sync rating to index card

--- website/movie/views.py
+++ website/movie/views.py
@@ -147,12 +147,17 @@ class IndexView(View):
         list_movies = {}
         all_movies = Movie.objects.all()[:4]
         all_genres = Genre.objects.all()
-        all_ratings = Rating.objects.all()
+        rating_dict = {}
+        for movie in all_movies:
+            rating_dict[movie.id] = Rating.objects.filter(movie=movie).aggregate(Avg('rating'))['rating__avg']
+
         for genre in all_genres:
             movie_in_genre = Movie.objects.filter(genre=genre)[:4]
             list_movies[genre] = movie_in_genre
             # list_movies.append(movie_in_genre)
-        return render(request,'index.html',{'all_movies':all_movies,'all_genres':all_genres, 'all_ratings': all_ratings, 'list_movies':list_movies})
+
+        print(rating_dict)
+        return render(request,'index.html',{'all_movies':all_movies,'all_genres':all_genres, 'list_movies':list_movies, 'rating_dict': rating_dict})
 
 
     def post(self, request):
