3bb4fc9880ce7ed10d0ce47c559efbca0ac58764
raksit <rak-sit@hotmail.com>
2016-11-26 17:43:52 +0700

    fixed test_login not using selenium

--- website/movie/tests.py
+++ website/movie/tests.py
@@ -1,13 +1,13 @@
 from django.test import TestCase, LiveServerTestCase, RequestFactory, Client
 from django.core.files import File
 from django.contrib.auth.models import User
+from django.contrib.sessions.middleware import SessionMiddleware
 from django.core.urlresolvers import reverse
 from .models import Genre, Movie, Rating, Serial
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
-from .views import filter, search_movie
-import time
+import json
+
+from .views import filter, search_movie, login_api, register_api
 
 # Create your tests here.
 
@@ -72,112 +72,33 @@ class BKLTestCase(TestCase):
         result  = response.context_data['selected_movies']
         self.assertEqual(len(expected), len(result))
 
-class BKLLiveTestCase(LiveServerTestCase):
-
-    # setUp is where you setup call fixture creation scripts
-    # and instantiate the WebDriver, which in turns loads up the browser.
-    def setUp(self):
-        self.selenium = webdriver.Chrome()
-        super(BKLLiveTestCase, self).setUp()
-
-    # Don't forget to call quit on your webdriver, so that
-    # the browser is closed after the tests are ran
-    def tearDown(self):
-        time.sleep(2)
-        self.selenium.quit()
-        super(BKLLiveTestCase, self).tearDown()
-
     def test_login(self):
-        selenium = self.selenium
-        selenium.get('http://127.0.0.1:8000/movie')
-        login_nav = selenium.find_element_by_link_text('Login')
-        login_nav.click()
-        username = selenium.find_element_by_id('login-username')
-        password = selenium.find_element_by_id('login-password')
-        error_message = selenium.find_element_by_id('login-error-message')
-        submit = selenium.find_element_by_xpath("//*[contains(text(), 'Sign in')]")
+        request = self.factory.get('/')
 
-        # need to wait until login modal is visible
-        time.sleep(2)
+        mutable = request.POST._mutable
+        request.POST._mutable = True
+        request.POST['username'] = 'bkl'
+        request.POST['password'] = 'incorrect'
+        request.POST._mutable = mutable
 
-        username.send_keys('admin')
-        password.send_keys('correct')
-        submit.click()
+        response = login_api(request)
 
-        # need to wait login process
-        time.sleep(1)
+        expected = 'Invalid username or password.'
+        result = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(expected, result['msg'])
 
-        self.assertEqual(error_message.text, "Invalid username or password.")
+        mutable = request.POST._mutable
+        request.POST._mutable = True
+        request.POST['username'] = 'bkl'
+        request.POST['password'] = 'password'
+        request.POST._mutable = mutable
 
-        username.clear()
-        password.clear()
+        # Annotate a request object with a session
+        middleware = SessionMiddleware()
+        middleware.process_request(request)
+        request.session.save()
 
-        username.send_keys('admin')
-        password.send_keys('incorrect')
-        submit.click()
-
-        # TODO: assert the satisfied result
-
-    def test_register(self):
-        selenium = self.selenium
-        selenium.get('http://127.0.0.1:8000/movie')
-        register_nav = selenium.find_element_by_link_text('Register')
-        register_nav.click()
-        username = selenium.find_element_by_id('register-username')
-        email = selenium.find_element_by_id('register-email')
-        password = selenium.find_element_by_id('register-password')
-        confirm_password = selenium.find_element_by_id('register-password-confirm')
-        check_policy = selenium.find_element_by_id('register-policy')
-        error_message = selenium.find_element_by_id('register-error-message')
-        submit = selenium.find_element_by_xpath("//*[contains(text(), 'Create Account')]")
-
-        # need to wait until register modal is visible
-        time.sleep(2)
-
-        username.send_keys('bkltestcase')
-        email.send_keys('bkltestcase@hotmail.com')
-        password.send_keys('password')
-        confirm_password.send_keys('drowssap')
-        check_policy.click()
-        submit.click()
-
-        # need to wait register process
-        time.sleep(1)
-
-        self.assertEqual(error_message.text, "Passwords are not the same")
-
-        username.clear()
-        email.clear()
-        password.clear()
-        confirm_password.clear()
-        check_policy.click()
-
-        username.send_keys('bkltestcase')
-        # email.send_keys('bkltestcase@hotmail.com')
-        password.send_keys('password')
-        confirm_password.send_keys('password')
-        check_policy.click()
-        submit.click()
-
-        # need to wait register process
-        time.sleep(1)
-
-        self.assertEqual(error_message.text, "Please input all the of fields")
-
-        username.clear()
-        email.clear()
-        password.clear()
-        confirm_password.clear()
-        check_policy.click()
-
-        username.send_keys('bkltestcase')
-        email.send_keys('bkltestcase@hotmail.com')
-        password.send_keys('password')
-        confirm_password.send_keys('password')
-        # check_policy.click()
-        submit.click()
-
-        # need to wait register process
-        time.sleep(1)
-
-        self.assertEqual(error_message.text, "Please accept our policy")
+        response = login_api(request)
+        expected = 'success'
+        result = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(expected, result['msg'])
